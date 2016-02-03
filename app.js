4d45dde48305f8053a0bc2edd7057baca9abde8a
Moni Francesca <mo.francesca@gmail.com>
2016-02-02 20:16:07 -0600

    initial commit

--- app.js
+++ app.js
@@ -1,24 +1,50 @@
-var atticus = ["Atticus", "2405", "47000", 3];
-var jem = ["Jem", "62347", "63500", 4];
-var boo = ["Boo", "11435", "54000", 3];
-var scout = ["Scout", "6243", "74750", 5];
+//create an employee constructor
+function Employee(name, empNumber, currentSalary, rating) {
+	this.name = name;
+	this.empNumber = empNumber;
+	this.currentSalary = currentSalary;
+	this.rating = rating;
+}
+
+//change the array declarations to object declarations
+var atticus = new Employee("Atticus", "2405", "47000", 3);
+var jem = new Employee("Jem", "62347", "63500", 4);
+var boo = new Employee("Boo", "11435", "54000", 3);
+var scout = new Employee("Scout", "6243", "74750", 5);
 
 var employees = [atticus, jem, boo, scout];
 
+//Create variables used to write to the DOM
+var newEl, newText, position;
+//Capture the position of insertion into the DOM
+position = document.getElementById('content');
+
+//Loop the array, extracting each array and writing information to the DOM
+//Note that the information is not 'clean'
+for(var i = 0; i < employees.length; i++){
+	employees[i] = calculateSTI(employees[i]);
+	newEl = document.createElement('li');
+	newText = document.createTextNode(employees[i][0] + ', ' + employees[i][1] + '%, $' + employees[i][2] + ', $' + employees[i][3]);
+	newEl.appendChild(newText);
+	position.appendChild(newEl);
+}
+
+
 function calculateSTI(empInfo) {
-	var name = empInfo[0];
-	var empNumber = empInfo[1];
-	var currentSalary = Math.round(parseFloat(empInfo[2]));
-	var rating = empInfo[3];
+	this.name = empInfo.name;
+	this.empNumber = empInfo.empNumber;
+	this.currentSalary = Math.round(parseFloat(empInfo.currentSalary));
+	this.rating = empInfo.rating;
+	console.log(empInfo);
 	
 	var processedEmployee = [];
 	var bonus = 0;
 	var bonusPercentage = 0;
-	var adjSalary = currentSalary;	// base + STI
+	var adjSalary = this.currentSalary;	// base + STI
 	var totalBonus = bonus;
 
 	// calc sti
-	switch(rating) {
+	switch(this.rating) {
 		case 0:
 		case 1:
 		case 2:
@@ -40,11 +66,11 @@ function calculateSTI(empInfo) {
 	bonusPercentage = adjustBonusPercentage(empNumber, bonusPercentage, currentSalary);
 	
 	// build processed array
-	processedEmployee[0] = name;
+	processedEmployee[0] = this.name;
 	processedEmployee[1] = bonusPercentage;
 
-	bonus = Math.round(bonusPercentage * currentSalary);
-	adjSalary = currentSalary + bonus;
+	bonus = Math.round(bonusPercentage * this.currentSalary);
+	adjSalary = this.currentSalary + bonus;
 
 	processedEmployee[2] = adjSalary;
 	processedEmployee[3] = bonus;
@@ -53,11 +79,11 @@ function calculateSTI(empInfo) {
 }
 
 function adjustBonusPercentage(empNumber, bonusPercentage, currentSalary) {
-	if(empNumber.length == 4) {
+	if(this.empNumber.length == 4) {
 		bonusPercentage += .05;
 	}
 
-	if(currentSalary > 65000) {
+	if(this.currentSalary > 65000) {
 		bonusPercentage -= .01;
 	}
 
